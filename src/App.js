899a990d92e2037dbbae526fb53ff3752e46333b
Ilya Flaks <105448137+ilyaflaks@users.noreply.github.com>
2022-11-08 10:12:51 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+//import { Axios, AxiosError } from "axios";
+import axios from "axios";
+import Card from "./components/card";
 function App() {
+  const [zipInput, setZipInput] = useState("");
+  const [zipQuery, setZipQuery] = useState("");
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState(null);
+  const [status, setStatus] = useState("");
+  let apiUrl = `https://data.epa.gov/efservice/getEnvirofactsUVHOURLY/ZIP/${zipQuery}/json`;
+
+  useEffect(() => {
+    console.log("url in useEff");
+    console.log(url);
+    axios
+      .get(url)
+      .then((response) => {
+        console.log("response.data: ");
+        console.log(response.data);
+        console.log(response.data[0].CITY);
+        console.log(response.data[0].DATE_TIME);
+
+        let newData = response.data;
+        if (typeof newData === "object") {
+          setData(newData);
+        }
+        let formattedDay = response.data[0].DATE_TIME.substring(0, 11).replace(
+          /\//g,
+          " "
+        );
+        console.log("formattedDay: " + formattedDay);
+        let resultStatus =
+          response.data[0].CITY +
+          ", " +
+          response.data[0].STATE +
+          " " +
+          formattedDay;
+        setStatus(resultStatus);
+        setZipInput("");
+        //setStatus("");
+      })
+      .catch(function (error) {
+        if (error.response.status === 404) {
+          console.log(error.response.status);
+          setStatus("No data found for your request");
+          setData(null);
+        }
+        //       setStatus("There was an error with your request");
+      });
+  }, [url]);
+
+  // ////THIS WORKS
+  //   useEffect(() => {
+  //     axios.get(apiUrl).then((response) => {
+  //       console.log(response.data);
+  //     });
+  //   }, []);
+  // ////
+
+  function onInputChange(e) {
+    setZipInput(e.target.value);
+    console.log(zipInput);
+  }
+
+  function onSubmit() {
+    const numRegex = /^\d+$/;
+    if (zipInput.length !== 5 || !numRegex.test(zipInput)) {
+      setStatus(
+        "Invalid ZIP code. Please enter 5 digits with no special characters"
+      );
+      setData(null);
+    } else {
+      setStatus("Loading...");
+      setUrl(
+        `https://data.epa.gov/efservice/getEnvirofactsUVHOURLY/ZIP/${zipInput}/json`
+      );
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          onChange={onInputChange}
+          placeholder="Enter a 5 digit ZIP code"
+          value={zipInput}
+        ></input>
+        <button onClick={onSubmit}>Submit</button>
+        <h3>{status}</h3>
+        {data &&
+          data.map((item, index) => {
+            return (
+              <Card
+                key={index}
+                date_time={item.DATE_TIME}
+                uv_value={item.UV_VALUE}
+              />
+            );
+          })}
       </header>
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <Card DATE_TIME={item.DATE_TIME} index={index} UV_VALUE={item.UV_VALUE}/> */
+}
+{
+  /* <div key={index}>
+                <h3>_____</h3>
+                <h3>{item.CITY}</h3>
+                <h3>{item.DATE_TIME}</h3>
+                <h3>{item.ZIP}</h3>
+                <h3>{item.UV_VALUE}</h3>
+              </div> */
+}
---
+++ src/components/card.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Card = (props) => {
+  const localTime = new Date();
+  // console.log("props date time: " + props.date_time);
+  const exactTime = localTime.toLocaleTimeString();
+  //  console.log("exactTime: " + exactTime);
+  const localHour = exactTime.substring(0, 1);
+  // console.log("localHour: " + localHour);
+  const localAmpm = exactTime.substring(8);
+  // console.log("localAmpm: " + localAmpm);
+  const localHourAmPm = localHour + " " + localAmpm;
+
+  let timeOfDay = props.date_time.substring(12);
+
+  if (timeOfDay.startsWith("0")) {
+    timeOfDay = timeOfDay.substring(1);
+  }
+  console.log("time of day: " + timeOfDay);
+  console.log("localHourAmPm: " + localHourAmPm);
+
+  //TODO
+  //add some logic to add styles based on local time of day. If the time has already passed, make it gray
+  //if the UV index is over 4, make it red or orange
+
+  ////Military time converter from HackerRank:
+  function timeConversion(s) {
+    let hour = s.substring(0, 2);
+    let ampm = s.substring(8);
+    let militaryHour = "";
+    if (ampm === "AM" && hour !== "12") {
+      militaryHour = hour;
+    } else if (ampm === "PM" && hour === "12") {
+      militaryHour = hour;
+    } else if (ampm === "AM" && hour === "12") {
+      militaryHour = "00";
+    } else {
+      militaryHour = parseInt(hour) + 12;
+    }
+    let minutes = s.substring(2, 8);
+    let militaryTime = militaryHour + minutes;
+    return militaryTime;
+  }
+  /////END MILITARY TIME CONVERTER
+
+  return (
+    <div className="uv-card">
+      <h3>{timeOfDay}</h3>
+      <h3>UV Index: {props.uv_value}</h3>
+    </div>
+  );
+};
+
+export default Card;
